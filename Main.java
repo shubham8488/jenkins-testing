5219d1cb7cddb1319db84b3396124102c033e030
Shubham Kumar <shubhkum@amdocs.com>
2024-01-22 02:43:43 +0530

    updated mian.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+package test1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Every , Java application is running successfully !!");
+
+	}
+
+}
